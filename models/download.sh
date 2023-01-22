d8347ad07f61bd89d0f73032eb5a593c330bbe6e
qlalf1457 <qlalf1457@gist.ac.kr>
2023-01-23 08:10:20 +0900

    v1

---
+++ models/download.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+wget https://zenodo.org/record/7559281/files/breast_model.tar.gz
+wget https://zenodo.org/record/7559281/files/brain_model.tar.gz
+tar -xvf breast_model.tar.gz
+tar -xvf brain_model.tar.gz
+
