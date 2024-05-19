388d6a3ae85b82d897e3ef9a0719bf6bb9267208
ylee <qlalf1457@gist.ac.kr>
2024-05-19 14:58:33 +0900

    wget -N

--- models/download.sh
+++ models/download.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 if [[ ! -s breast_model ]];then
-	wget https://zenodo.org/records/10544099/files/breast_model.tar.gz
+	wget -N https://zenodo.org/records/10544099/files/breast_model.tar.gz
 	tar -xvf breast_model.tar.gz
 fi
 
 if [[ ! -s breast_model_1Mb ]];then
-	wget https://zenodo.org/records/11201552/files/breast_model_1Mb.tar.gz
+	wget -N https://zenodo.org/records/11201552/files/breast_model_1Mb.tar.gz
 	tar -xvf breast_model_1Mb.tar.gz
 
 fi
 
 
 if [[ ! -s brain_model ]];then
-	wget https://zenodo.org/records/10544099/files/brain_model.tar.gz
+	wget -N https://zenodo.org/records/10544099/files/brain_model.tar.gz
 	tar -xvf brain_model.tar.gz
 fi
 
 if [[ ! -s brain_model_1Mb ]];then
-	wget https://zenodo.org/records/11201557/files/brain_model_1Mb.tar.gz
+	wget -N https://zenodo.org/records/11201557/files/brain_model_1Mb.tar.gz
 	tar -xvf brain_model_1Mb.tar.gz
 fi
